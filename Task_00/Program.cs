47e6bceb3de0bf445f5abb7067c6e7115ff79b85
Andrey Sharganov <sharganow@gmail.com>
2022-06-24 01:45:30 +0500

    Переделали в задачу вычисления квадрата

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,3 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// 
+Console.Write("Enter a number to calculate the square of this number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Here the square is: {square}");
 Console.Beep();
