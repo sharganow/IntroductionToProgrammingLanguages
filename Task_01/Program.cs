0d26af2a59f88bea9911899189888e605a880186
Andrey Sharganov <sharganow@gmail.com>
2022-06-24 14:22:14 +0500

    Выполнил самостоятельно вне классного часа, сдаю как домашнюю работу

---
+++ Task_01/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+Console.Beep(); Console.Beep(); Console.Beep();
+string quit;
+char quitRepite = 'n';
+int A; int B; int squareB;
+do{
+    Console.WriteLine("Let's check: is the number A the square of the number B?");
+    Console.Write("Enter number A: ");
+    A = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter number B: ");
+    B = Convert.ToInt32(Console.ReadLine());
+    squareB = B * B;
+    if(squareB == A){
+        Console.WriteLine("Yes, the number A is the square of the number B!");
+    }
+    else{
+        Console.WriteLine("No, the number A is not the square of the number B!");
+    }
+    Console.WriteLine("Would you like to continue? If yes, then click 'Y'");
+    quit = Console.ReadLine();
+    quit = quit.ToLower();
+    if('y' == Convert.ToChar(quit)){
+        quitRepite = 'y';
+    }
+    else{
+        Console.WriteLine("What will we continue");
+    };
+
+}while(quitRepite == 'n');
+ Console.WriteLine("We will be glad to see you again!");
+ Console.Beep(); Console.Beep();
