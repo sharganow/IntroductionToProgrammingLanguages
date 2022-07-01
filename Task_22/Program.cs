ff044dc23e1d35b61fd0384ebce85266d1061430
Andrey Sharganov <sharganow@gmail.com>
2022-07-01 14:29:29 +0500

    Task_22 mode1

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -4,17 +4,25 @@
 //      5 -> 1, 4, 9, 16, 25
 //      2 -> 1, 4
 
-string strQuarterRange;
-Console.Write("Enter number N:");
-int intN = Convert.ToInt32(Console.ReadLine());
-strQuarterRange = MakeQuarterRange (intN);
-Console.WriteLine(strQuarterRange);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string strQuarterRange;
+        Console.Write("Enter number N:");
+        int intN = Convert.ToInt32(Console.ReadLine());
+        strQuarterRange = MakeQuarterRange(intN);
+        Console.WriteLine(strQuarterRange);
 
-string MakeQuarterRange(int number){
-    string QuarterRange = Convert.ToString(number) + " -> ";
-    int i = 1;
-    while(i <= number){
-        QuarterRange += Convert.ToString(Math.Pow(i++, 2)) + " ";
+        string MakeQuarterRange(int number)
+        {
+            string QuarterRange = Convert.ToString(number) + " -> ";
+            int i = 1;
+            while (i <= number)
+            {
+                QuarterRange += Convert.ToString(Math.Pow(i++, 2)) + " ";
+            }
+            return QuarterRange;
+        }
     }
-    return QuarterRange;
 }
