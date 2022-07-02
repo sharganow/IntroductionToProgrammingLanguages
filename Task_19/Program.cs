07f6d7e92b6156fa6d90de450a2bf1ceb79acf2f
sharganow <sharganow@gmail.com>
2022-07-02 21:44:05 +0500

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №19
+// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом
+
+string strTestPalindrome = null;
+Console.Write("Please, enter five character number: ");
+strTestPalindrome = Console.ReadLine();
+Console.Write   (
+                    TestPalindrome(strTestPalindrome)
+                );
+
+
+string TestPalindrome(string strTestString){
+    int i, j;
+    j = strTestString.Length - 1;
+    if(j != 4){
+        // return "charecters must be just five, no more or less";
+        Console.WriteLine("According to the condition of the Task, the entered characters must be five");
+    }
+    string strAnswer;
+    bool bPalindrome;
+    for(i = 0, bPalindrome = true; i < j; i++, j--){
+        if( strTestString[i] != strTestString[j]){
+            bPalindrome = false;
+            break;
+        }
+    }
+    strAnswer = strTestString + " -> ";
+    strAnswer += bPalindrome ? "Yes, It's palindrome" : "No, it isn't palindrome";
+    return strAnswer;
+}
