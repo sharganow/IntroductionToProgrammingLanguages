14c67655d92bf65980a69537cad70c01302fabf4
Andrey Sharganov <sharganow@gmail.com>
2022-07-04 22:47:39 +0500

    Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №28
+// Напишите программу, которая принимает на вход число N и 
+// выдаёт  произведение чисел от 1 до N
+// 4 -> 24
+// 5 -> 120
+Console.Write("Введите любое число: ");
+int enteredNumber = int.Parse(Console.ReadLine());
+int result = Fractal(enteredNumber);
+Console.WriteLine($"Вычисленное значение от введенного числа {enteredNumber} по условию равно {result}");
+
+int Fractal(int number){
+    int answer = 1;
+    for(int i = 1; i <= number; i++){
+        answer *= i;
+    }
+    return answer;
+}
+
