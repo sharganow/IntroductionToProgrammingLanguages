7f1a08aec3f7b823db32b1cd56837c61ef7b918d
Andrey Sharganov <sharganow@gmail.com>
2022-07-12 00:06:11 +0500

    Task_45 copy massive

--- Task_45/Program.cs
+++ Task_45/Program.cs
@@ -1,7 +1,56 @@
-﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
-// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
-// Если N = 5 -> 0 1 1 2 3
-// Если N = 3 -> 0 1 1
-// Если N = 7 -> 0 1 1 2 3 5 8
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
 
-Console.WriteLine("Hello, World!");
+
+Random rnd = new Random();
+int size = rnd.Next(2, 11);
+int[]   randomeMassive;
+int[]   copyMassive;
+double     amplitude;
+string  viewAnswer;
+
+randomeMassive   = FillRandomeMassive(size, -100, 100);
+copyMassive      = CopyMassive(randomeMassive);
+viewAnswer       = ShowResultReverseElements(randomeMassive, copyMassive);
+Console.WriteLine( viewAnswer );
+
+int[] FillRandomeMassive(int size, int lowLimit, int highLimit){
+    int[] massive = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < massive.Length; i++){
+        massive[i] = rnd.Next(lowLimit, highLimit + 1);
+    }
+    return massive;
+}
+
+int[] CopyMassive(int[] mssv){
+    int[] copy = new int[mssv.Length];
+    for(int i = 0; i < mssv.Length; i++){
+        if(i == 1){
+            copy[i] = 444;
+        }
+        else{
+            copy[i] = mssv[i];
+        }
+    }
+    return copy;
+}
+
+string ShowResultReverseElements(int[] massive, int[] antiMassive){
+    string viewMassive = MakeViewStringMassive(massive);
+    string viewResult  = MakeViewStringMassive(antiMassive);
+
+    return viewMassive + " -> " + viewResult;
+}
+
+string MakeViewStringMassive(int[] massive){
+    string viewString = "[";
+    int j = 1;
+    for(int i = 0; i < massive.Length; i++, j++){
+        viewString += Convert.ToString(massive[i]);
+        if(j != massive.Length) viewString += ", ";
+    }
+    viewString += "]";
+    return viewString;
+}
