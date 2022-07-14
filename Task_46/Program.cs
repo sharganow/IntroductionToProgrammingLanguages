c6f509b2b4fa57d10fedf47447d074c30bbb9f5f
Andrey Sharganov <sharganow@gmail.com>
2022-07-14 20:02:44 +0500

    Task_46 complite

--- Task_46/Program.cs
+++ Task_46/Program.cs
@@ -1,10 +1,14 @@
-﻿
+﻿// Задача №46:
+// Задайте двумерный массив размером m x n,
+// заполненный случайными целыми числами
+
+
 int [,] matrix;
 
-matrix = FillMatrixRndInt(3, 4, 9, 0);
-PrintMatrixRndInt(matrix);
+matrix = FillMatrixRndInt(3, 4, -9, 9);
+PrintMatrixInt(matrix);
 
-int[,] FillMatrixRndInt(int row, int col, int max, int min){
+int[,] FillMatrixRndInt(int row, int col, int min, int max){
     int[,] mssv = new int[row, col];
     Random rnd = new Random();
     for(int i = 0; i < mssv.GetLength(0); i++){
@@ -15,14 +19,13 @@ int[,] FillMatrixRndInt(int row, int col, int max, int min){
     return mssv;
 }
 
-void PrintMatrixRndInt(int[,] mssv){
+void PrintMatrixInt(int[,] mssv){
     for(int i = 0; i < mssv.GetLength(0); i++){
         Console.Write("[");
         for(int j = 0; j < mssv.GetLength(1); j++){
-            Console.Write(mssv[i,j]);
+            Console.Write($"{mssv[i,j], 3}");
             if(j < (mssv.GetLength(1) -1)){   Console.Write(", ");}
         }
-        Console.WriteLine("]");
+        Console.WriteLine("  ]");
     }
 }
-
