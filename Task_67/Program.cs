ed966af2a5aa22534bf07cf008718cdd4aa9aeba
sharganow <sharganow@gmail.com>
2022-07-26 17:33:33 +0500

    Task_67

---
+++ Task_67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №67:
+// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его чисел.
+
+Console.Clear();
+Console.Write("Set number: ");
+int num = int.Parse(Console.ReadLine());
+int sum;
+
+sum             = SumDigitalNumber  ( num );
+         Console. WriteLine         ( $"{num} -> {sum}" );
+
+int SumDigitalNumber(int num){
+    if(num == 0) return 0;
+    else{
+        return SumDigitalNumber(num/10) + num%10;
+    }
+}
