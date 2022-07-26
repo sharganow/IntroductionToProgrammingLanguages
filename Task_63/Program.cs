a62bb1418981815a51d3535c428cfeb968f2bf8c
sharganow <sharganow@gmail.com>
2022-07-26 15:56:15 +0500

    Task_63

---
+++ Task_63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №63:
+// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке
+// от 1 до N.
+
+Console.Clear();
+Console.Write("Set number: ");
+int num = int.Parse(Console.ReadLine());
+
+NaturalNumber(num);
+
+void NaturalNumber(int num){
+    if(num == 0) return;
+    else{
+        Console.Write($"{num}");
+        NaturalNumber(num - 1);
+    }
+}
